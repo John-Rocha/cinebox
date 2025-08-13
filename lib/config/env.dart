8dd0e3c90ad0f468d9dd3e2baa5113caf9092213
Johnathan Rocha <johnathanrocha@gmail.com>
2025-08-13 13:26:56 -0300

    fix: change backendBaseUrl from final to const for better performance

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -8,7 +8,7 @@ abstract class Env {
   static final String googleApiKey = _Env.googleApiKey;
 
   @EnviedField(varName: 'BACKEND_BASE_URL')
-  static final String backendBaseUrl = _Env.backendBaseUrl;
+  static const String backendBaseUrl = _Env.backendBaseUrl;
 
   @EnviedField(varName: 'THE_MOVIE_DB_API_KEY', obfuscate: true)
   static final String theMovieDbApiKey = _Env.theMovieDbApiKey;
