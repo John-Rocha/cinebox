70234fd3d5fa663880e467c898e1a7ba44a4ab1e
Johnathan Rocha <johnathanrocha@gmail.com>
2025-10-05 08:00:54 -0300

    feat: Implement loader messages mixin and integrate loading animation widget

---
+++ lib/ui/core/widgets/loader_messages.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+mixin LoaderMessages<T extends ConsumerStatefulWidget> on ConsumerState<T> {
+  var isOpen = false;
+
+  void showLoader() {
+    if (!isOpen) {
+      isOpen = true;
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (_) => LoadingAnimationWidget.threeArchedCircle(
+          color: Colors.white,
+          size: 60,
+        ),
+      );
+    }
+  }
+
+  void hideLoader() {
+    if (isOpen) {
+      isOpen = false;
+      Navigator.of(context).pop();
+    }
+  }
+
+  void _showSnackbar(String message, Color backgroundColor) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: backgroundColor,
+      ),
+    );
+  }
+
+  void showErrorSnackbar(String message) => _showSnackbar(message, Colors.red);
+
+  void showSuccessSnackbar(String message) =>
+      _showSnackbar(message, Colors.green);
+
+  void showInfoSnackbar(String message) => _showSnackbar(message, Colors.blue);
+}
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,15 +10,25 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _State();
 }
 
-class _State extends ConsumerState<SplashScreen> {
+class _State extends ConsumerState<SplashScreen> with LoaderMessages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Image.asset(
           R.ASSETS_IMAGES_BG_LOGIN_PNG,
-          fit: BoxFit.cover,
+          fit: BoxFit.fill,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // showLoader();
+          // Future.delayed(const Duration(seconds: 3), () {
+          //   hideLoader();
+          //   showErrorSnackbar("An error occurred");
+          // });
+        },
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
