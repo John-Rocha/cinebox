e4151a2b31dc4700c3e0665282fcbd80ed3534d5
Johnathan Rocha <johnathanrocha@gmail.com>
2025-11-04 17:04:02 -0300

    feat: implement GetFavoriteCommand and integrate with FavoritesViewModel and FavoritesScreen

--- lib/ui/favorites/favorites_screen.dart
+++ lib/ui/favorites/favorites_screen.dart
@@ -1,15 +1,76 @@
+import 'package:cinebox/ui/core/widgets/movie_card.dart';
+import 'package:cinebox/ui/favorites/commands/get_favorite_command.dart';
+import 'package:cinebox/ui/favorites/favorites_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends ConsumerStatefulWidget {
   const FavoritesScreen({super.key});
 
   @override
+  ConsumerState<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends ConsumerState<FavoritesScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(favoritesViewModelProvider).fetchFavorites();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(getFavoriteCommandProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus favoritos'),
       ),
-      body: Container(),
+      body: favoritesMovies.when(
+        data: (movies) {
+          if (movies.isEmpty) {
+            return const Center(
+              child: Text('Nenhum filme favorito encontrado.'),
+            );
+          }
+          return CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 148,
+                    mainAxisExtent: 268,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    childCount: movies.length,
+                    (context, index) {
+                      final movie = movies[index];
+                      return Container(
+                        margin: EdgeInsets.all(8),
+                        child: MovieCard(
+                          key: UniqueKey(),
+                          id: movie.id,
+                          title: movie.title,
+                          year: movie.year,
+                          imageUrl: movie.posterPath,
+                          isFavorite: true,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(
+          child: Text('Erro ao carregar favoritos'),
+        ),
+      ),
     );
   }
 }
